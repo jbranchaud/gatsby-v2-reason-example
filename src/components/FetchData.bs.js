2e560db36837dd09c58b48dc2412dda6a9dc996e
jbranchaud <jbranchaud@gmail.com>
2018-10-11 13:05:53 -0500

    Add pagination for viewing additional characters

--- src/components/FetchData.bs.js
+++ src/components/FetchData.bs.js
@@ -9,6 +9,18 @@ var Curry = require("bs-platform/lib/js/curry.js");
 var React = require("react");
 var Json_decode = require("@glennsl/bs-json/src/Json_decode.bs.js");
 var ReasonReact = require("reason-react/src/ReasonReact.js");
+var Js_primitive = require("bs-platform/lib/js/js_primitive.js");
+
+function responseInfo(json) {
+  var url = Json_decode.field("prev", Json_decode.string, json);
+  var tmp = url === "" ? undefined : url;
+  var url$1 = Json_decode.field("next", Json_decode.string, json);
+  var tmp$1 = url$1 === "" ? undefined : url$1;
+  return /* record */[
+          /* prevPageUrl */tmp,
+          /* nextPageUrl */tmp$1
+        ];
+}
 
 function character(json) {
   return /* record */[
@@ -25,10 +37,14 @@ function characters(json) {
 }
 
 function results(json) {
-  return /* record */[/* results */Json_decode.field("results", characters, json)];
+  return /* record */[
+          /* results */Json_decode.field("results", characters, json),
+          /* info */Json_decode.field("info", responseInfo, json)
+        ];
 }
 
 var Decode = /* module */[
+  /* responseInfo */responseInfo,
   /* character */character,
   /* characters */characters,
   /* results */results
@@ -38,10 +54,30 @@ function fetchCharacters() {
   return fetch("/api/character/").then((function (prim) {
                     return prim.json();
                   })).then((function (json) {
-                  var results$1 = results(json);
-                  return Promise.resolve(results$1[/* results */0]);
+                  return Promise.resolve(results(json));
                 })).catch((function () {
                 return Promise.resolve(undefined);
+              }));
+}
+
+function fetchPage(pageUrl, send) {
+  var parts = pageUrl.split("rickandmortyapi.com");
+  var apiUrl = List.nth($$Array.to_list(parts), 1);
+  return fetch(apiUrl).then((function (prim) {
+                      return prim.json();
+                    })).then((function (json) {
+                    return Promise.resolve(results(json));
+                  })).catch((function () {
+                  return Promise.resolve(undefined);
+                })).then((function (result) {
+                if (result !== undefined) {
+                  var rickAndMortyResponse = result;
+                  var characters = rickAndMortyResponse[/* results */0];
+                  Curry._1(send, /* SetCharacters */Block.__(0, [characters]));
+                  return Promise.resolve(Curry._1(send, /* SetResponseInfo */Block.__(1, [rickAndMortyResponse[/* info */1]])));
+                } else {
+                  return Promise.resolve(Curry._1(send, /* SetLoadingFailed */Block.__(2, ["Failed to load some characters."])));
+                }
               }));
 }
 
@@ -69,6 +105,18 @@ function listCharacters(characters) {
   }
 }
 
+function $$default(value, optional) {
+  if (optional !== undefined) {
+    return Js_primitive.valFromOption(optional);
+  } else {
+    return value;
+  }
+}
+
+function exists(optional) {
+  return optional !== undefined;
+}
+
 var component = ReasonReact.reducerComponent("FetchData");
 
 function make() {
@@ -81,9 +129,12 @@ function make() {
               console.log("We are mounting!");
               fetchCharacters(/* () */0).then((function (result) {
                       if (result !== undefined) {
-                        return Promise.resolve(Curry._1(self[/* send */3], /* SetCharacters */Block.__(0, [result])));
+                        var rickAndMortyResponse = result;
+                        var characters = rickAndMortyResponse[/* results */0];
+                        Curry._1(self[/* send */3], /* SetCharacters */Block.__(0, [characters]));
+                        return Promise.resolve(Curry._1(self[/* send */3], /* SetResponseInfo */Block.__(1, [rickAndMortyResponse[/* info */1]])));
                       } else {
-                        return Promise.resolve(Curry._1(self[/* send */3], /* SetLoadingFailed */Block.__(1, ["Failed to load some characters."])));
+                        return Promise.resolve(Curry._1(self[/* send */3], /* SetLoadingFailed */Block.__(2, ["Failed to load some characters."])));
                       }
                     }));
               return /* () */0;
@@ -93,35 +144,67 @@ function make() {
           /* willUpdate */component[/* willUpdate */7],
           /* shouldUpdate */component[/* shouldUpdate */8],
           /* render */(function (self) {
+              var hasPrevious = exists(self[/* state */1][/* responseInfo */2][/* prevPageUrl */0]);
+              var hasNext = exists(self[/* state */1][/* responseInfo */2][/* nextPageUrl */1]);
+              var prevUrl = $$default("", self[/* state */1][/* responseInfo */2][/* prevPageUrl */0]);
+              var nextUrl = $$default("", self[/* state */1][/* responseInfo */2][/* nextPageUrl */1]);
               var match = self[/* state */1][/* status */0];
-              if (typeof match === "number") {
-                if (match !== 0) {
-                  return React.createElement("div", undefined, listCharacters(self[/* state */1][/* characters */1]));
-                } else {
-                  return React.createElement("p", undefined, ReasonReact.element(undefined, undefined, Str.make("We are fetching some characters...", /* array */[])));
-                }
-              } else {
-                return React.createElement("p", undefined, ReasonReact.element(undefined, undefined, Str.make(match[0], /* array */[])));
-              }
+              return React.createElement("div", undefined, React.createElement("span", undefined, React.createElement("button", {
+                                  disabled: !hasPrevious,
+                                  onClick: (function () {
+                                      fetchPage(prevUrl, self[/* send */3]);
+                                      return /* () */0;
+                                    })
+                                }, ReasonReact.element(undefined, undefined, Str.make("«" + " previous", /* array */[]))), ReasonReact.element(undefined, undefined, Str.make(" ... ", /* array */[])), React.createElement("button", {
+                                  disabled: !hasNext,
+                                  onClick: (function () {
+                                      fetchPage(nextUrl, self[/* send */3]);
+                                      return /* () */0;
+                                    })
+                                }, ReasonReact.element(undefined, undefined, Str.make("next " + "»", /* array */[])))), typeof match === "number" ? (
+                            match !== 0 ? React.createElement("div", undefined, listCharacters(self[/* state */1][/* characters */1])) : React.createElement("p", undefined, ReasonReact.element(undefined, undefined, Str.make("We are fetching some characters...", /* array */[])))
+                          ) : React.createElement("p", undefined, ReasonReact.element(undefined, undefined, Str.make(match[0], /* array */[]))));
             }),
           /* initialState */(function () {
               return /* record */[
                       /* status : Loading */0,
-                      /* characters : [] */0
+                      /* characters : [] */0,
+                      /* responseInfo : record */[
+                        /* prevPageUrl */undefined,
+                        /* nextPageUrl */undefined
+                      ]
                     ];
             }),
           /* retainedProps */component[/* retainedProps */11],
-          /* reducer */(function (action, _) {
-              if (action.tag) {
+          /* reducer */(function (action, state) {
+              if (typeof action === "number") {
                 return /* Update */Block.__(0, [/* record */[
-                            /* status : Error */[action[0]],
-                            /* characters : [] */0
+                            /* status : Loading */0,
+                            /* characters */state[/* characters */1],
+                            /* responseInfo */state[/* responseInfo */2]
                           ]]);
               } else {
-                return /* Update */Block.__(0, [/* record */[
-                            /* status : Ready */1,
-                            /* characters */action[0]
-                          ]]);
+                switch (action.tag | 0) {
+                  case 0 : 
+                      return /* Update */Block.__(0, [/* record */[
+                                  /* status : Ready */1,
+                                  /* characters */action[0],
+                                  /* responseInfo */state[/* responseInfo */2]
+                                ]]);
+                  case 1 : 
+                      return /* Update */Block.__(0, [/* record */[
+                                  /* status */state[/* status */0],
+                                  /* characters */state[/* characters */1],
+                                  /* responseInfo */action[0]
+                                ]]);
+                  case 2 : 
+                      return /* Update */Block.__(0, [/* record */[
+                                  /* status : Error */[action[0]],
+                                  /* characters : [] */0,
+                                  /* responseInfo */state[/* responseInfo */2]
+                                ]]);
+                  
+                }
               }
             }),
           /* jsElementWrapped */component[/* jsElementWrapped */13]
@@ -130,7 +213,12 @@ function make() {
 
 exports.Decode = Decode;
 exports.fetchCharacters = fetchCharacters;
+exports.fetchPage = fetchPage;
 exports.listCharacters = listCharacters;
+exports.$$default = $$default;
+exports.default = $$default;
+exports.__esModule = true;
+exports.exists = exists;
 exports.component = component;
 exports.make = make;
 /* component Not a pure module */
--- src/components/FetchData.re
+++ src/components/FetchData.re
@@ -47,10 +47,30 @@ type character = {
   imageUrl: string,
 };
 
-type rickAndMortyResponse = {results: list(character)};
+type responseInfo = {
+  prevPageUrl: option(string),
+  nextPageUrl: option(string),
+};
+
+type rickAndMortyResponse = {
+  results: list(character),
+  info: responseInfo,
+};
 
 module Decode = {
   open Json.Decode;
+  let responseInfo = json: responseInfo => {
+    prevPageUrl:
+      switch (field("prev", string, json)) {
+      | "" => None
+      | url => Some(url)
+      },
+    nextPageUrl:
+      switch (field("next", string, json)) {
+      | "" => None
+      | url => Some(url)
+      },
+  };
   let character = json: character => {
     id: field("id", int, json),
     name: field("name", string, json),
@@ -60,6 +80,7 @@ module Decode = {
   };
   let characters = json: list(character) => list(character, json);
   let results = json: rickAndMortyResponse => {
+    info: field("info", responseInfo, json),
     results: field("results", characters, json),
   };
 };
@@ -68,11 +89,14 @@ module Decode = {
 type state = {
   status,
   characters: list(character),
+  responseInfo,
 };
 
 /* Action declaration */
 type action =
   | SetCharacters(list(character))
+  | SetResponseInfo(responseInfo)
+  | SetLoading
   | SetLoadingFailed(string);
 
 let fetchCharacters = () =>
@@ -80,13 +104,34 @@ let fetchCharacters = () =>
     Fetch.fetch("/api/character/")
     |> then_(Fetch.Response.json)
     |> then_(json =>
-         json
-         |> Decode.results
-         |> (results => Some(results.results))
-         |> resolve
+         json |> Decode.results |> (results => Some(results)) |> resolve
        )
     |> catch(_err => None |> resolve)
   );
+
+let fetchPage = (pageUrl, send) => {
+  let parts = Js.String.split("rickandmortyapi.com", pageUrl);
+  let apiUrl = List.nth(parts |> Array.to_list, 1);
+  Js.Promise.(
+    Fetch.fetch(apiUrl)
+    |> then_(Fetch.Response.json)
+    |> then_(json =>
+         json |> Decode.results |> (results => Some(results)) |> resolve
+       )
+    |> catch(_err => None |> resolve)
+    |> then_(result =>
+         switch (result) {
+         | Some(rickAndMortyResponse) =>
+           let characters = rickAndMortyResponse.results;
+           send(SetCharacters(characters));
+           send(SetResponseInfo(rickAndMortyResponse.info)) |> resolve;
+         | None =>
+           send(SetLoadingFailed("Failed to load some characters."))
+           |> resolve
+         }
+       )
+  );
+};
 
 let listCharacters = (characters: list(character)) =>
   switch (characters) {
@@ -118,6 +163,18 @@ let listCharacters = (characters: list(character)) =>
     </ul>
   };
 
+let default = (value, optional) =>
+  switch (optional) {
+  | None => value
+  | Some(value) => value
+  };
+
+let exists = optional =>
+  switch (optional) {
+  | None => false
+  | Some(_) => true
+  };
+
 /* Component template declaration.
    Needs to be **after** state and action declarations! */
 let component = ReasonReact.reducerComponent("FetchData");
@@ -127,15 +184,24 @@ let component = ReasonReact.reducerComponent("FetchData");
 let make = _children => {
   /* spread the other default fields of component here and override a few */
   ...component,
-  initialState: () => {status: Loading, characters: []},
+  initialState: () => {
+    status: Loading,
+    characters: [],
+    responseInfo: {
+      prevPageUrl: None,
+      nextPageUrl: None,
+    },
+  },
   didMount: self => {
     Js.log("We are mounting!");
     Js.Promise.(
       fetchCharacters()
       |> then_(result =>
            switch (result) {
-           | Some(characters) =>
-             self.send(SetCharacters(characters)) |> resolve
+           | Some(rickAndMortyResponse) =>
+             let characters = rickAndMortyResponse.results;
+             self.send(SetCharacters(characters));
+             self.send(SetResponseInfo(rickAndMortyResponse.info)) |> resolve;
            | None =>
              self.send(SetLoadingFailed("Failed to load some characters."))
              |> resolve
@@ -149,13 +215,39 @@ let make = _children => {
     switch (action) {
     | SetCharacters(characters) =>
       ReasonReact.Update({...state, characters, status: Ready})
+    | SetResponseInfo(responseInfo) =>
+      ReasonReact.Update({...state, responseInfo})
     | SetLoadingFailed(msg) =>
       ReasonReact.Update({...state, characters: [], status: Error(msg)})
+    | SetLoading => ReasonReact.Update({...state, status: Loading})
     },
-  render: self =>
-    switch (self.state.status) {
-    | Loading => <p> <Str s="We are fetching some characters..." /> </p>
-    | Error(msg) => <p> <Str s=msg /> </p>
-    | Ready => <div> {listCharacters(self.state.characters)} </div>
-    },
+  render: self => {
+    let hasPrevious = exists(self.state.responseInfo.prevPageUrl);
+    let hasNext = exists(self.state.responseInfo.nextPageUrl);
+    let prevUrl = default("", self.state.responseInfo.prevPageUrl);
+    let nextUrl = default("", self.state.responseInfo.nextPageUrl);
+
+    <div>
+      <span>
+        <button
+          disabled={!hasPrevious}
+          onClick={_e => fetchPage(prevUrl, self.send) |> ignore}>
+          <Str s={{js|«|js} ++ " previous"} />
+        </button>
+        <Str s=" ... " />
+        <button
+          disabled={!hasNext}
+          onClick={_e => fetchPage(nextUrl, self.send) |> ignore}>
+          <Str s={"next " ++ {js|»|js}} />
+        </button>
+      </span>
+      {
+        switch (self.state.status) {
+        | Loading => <p> <Str s="We are fetching some characters..." /> </p>
+        | Error(msg) => <p> <Str s=msg /> </p>
+        | Ready => <div> {listCharacters(self.state.characters)} </div>
+        }
+      }
+    </div>;
+  },
 };
